2684ffdc124e0f87e0a568187ca99aa5a59623ac
EUGEarg <eugeniarivarossa@gmail.com>
2022-05-31 20:22:21 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d24e823a22421db1d9c3a251a78853c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '361e669b-429f-48ea-a8c4-4d0335b56e9f'
+};
